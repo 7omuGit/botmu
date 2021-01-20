4efbd033caf5fe7e862684d8af9496f22cd5f9b1
Tom Zhang <zhangtom2088@gmail.com>
2021-01-20 13:41:22 -0500

    first commit

--- index.js
+++ index.js
@@ -10,16 +10,16 @@ bot.on('ready', () => {
 });
 
 bot.on('message', msg => {
-  if (msg.content === 'ping') {
-    msg.reply('pong');
-    msg.channel.send('pong');
+  if (msg.content === '!goosey') {
+    msg.channel.send('https://cdn.discordapp.com/emojis/697995591921172532.gif');
 
-  } else if (msg.content.startsWith('!kick')) {
-    if (msg.mentions.users.size) {
-      const taggedUser = msg.mentions.users.first();
-      msg.channel.send(`You wanted to kick: ${taggedUser.username}`);
-    } else {
-      msg.reply('Please tag a valid user!');
-    }
-  }
+  } 
+  //else if (msg.content.startsWith('!kick')) {
+  //   if (msg.mentions.users.size) {
+  //     const taggedUser = msg.mentions.users.first();
+  //     msg.channel.send(`You wanted to kick: ${taggedUser.username}`);
+  //   } else {
+  //     msg.reply('Please tag a valid user!');
+  //   }
+  // }
 });
